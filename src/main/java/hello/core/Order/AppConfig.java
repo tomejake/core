7535db8a2e73cb9a3e318af8768fbe17a1715a3d
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2021-12-21 23:59:23 +0900

    테스트 코드 수정

--- src/main/java/hello/core/Order/AppConfig.java
+++ src/main/java/hello/core/Order/AppConfig.java
@@ -1,13 +1,18 @@
 package hello.core.Order;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
 
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImp(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 
 }
