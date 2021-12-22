51f03c616694acfe6feff7cfad72eb5617846110
tomejake <tomejake22@gmail.com>
2021-12-22 11:25:51 +0900

    할인 정책 변경

--- src/main/java/hello/core/Order/AppConfig.java
+++ src/main/java/hello/core/Order/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -22,7 +23,7 @@ public class AppConfig {
     }
 
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
