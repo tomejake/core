f0ea3b58327578b44cc265bebe0a4ae9513c774d
tomejake <tomejake22@gmail.com>
2021-12-23 10:56:54 +0900

    스프링 코드로 변경

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import hello.core.Order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService =  applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getId());
+        System.out.println("member = " + member.getId());
+    }
+}
