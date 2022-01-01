5d28132aa42fd4a00ab4f7973077976ca8695a91
JunhyeokYu <tomejake22@gmail.com>
2022-01-01 23:51:50 +0900

    고객 요청 log작성

---
+++ src/main/java/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURI = request.getRequestURI();
+        myLogger.setRequestURL(requestURI);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
